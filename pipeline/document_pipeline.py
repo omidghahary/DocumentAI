90bacbef5c60600219a5563806fc5f2d9bebcb50
Omid Ghahari <omid.ghahary@gmail.com>
2026-07-31 16:23:28 +0330

    Implement document processing pipeline

--- pipeline/document_pipeline.py
+++ pipeline/document_pipeline.py
@@ -1,17 +1,22 @@
 from pipeline.base_pipeline import BasePipeline
 from models.document_model import DocumentModel
 from models.pipeline_result_model import PipelineResultModel
+from images.base_image_extractor import BaseImageExtractor
+from ocr.base_document_ocr import BaseDocumentOCR
+from chunking.base_chunker import BaseChunker
+from prompting.base_prompt_builder import BasePromptBuilder
+from llm.base_llm import BaseLLM
 
 class DocumentPipeline(BasePipeline):
 
     def __init__(
         self,
-        image_extractor,
-        ocr,
-        chunker,
-        prompt_builder,
-        llm,
-    ):
+        image_extractor: BaseImageExtractor,
+        ocr: BaseDocumentOCR,
+        chunker: BaseChunker,
+        prompt_builder: BasePromptBuilder,
+        llm: BaseLLM,
+        ):
         self.image_extractor = image_extractor
         self.ocr = ocr
         self.chunker = chunker
@@ -19,4 +24,20 @@ class DocumentPipeline(BasePipeline):
         self.llm = llm
         
     def process(self, document: DocumentModel) -> PipelineResultModel:
-        raise NotImplementedError
+        document = self.image_extractor.extract_images(document)
+        document = self.ocr.extract_document_text(document)
+        chunks = self.chunker.chunk(document)
+        prompts = [
+            self.prompt_builder.build(chunk)
+            for chunk in chunks
+        ]
+        responses = [
+            self.llm.generate(prompt)
+            for prompt in prompts
+        ]
+        return PipelineResultModel(
+            document= document,
+            chunks= chunks,
+            prompts= prompts,
+            responses= responses
+        )
--- tests/test_document_pipeline.py
+++ tests/test_document_pipeline.py
@@ -1,57 +1,127 @@
-import pytest
+# import pytest
+from unittest.mock import Mock
 from pipeline.document_pipeline import DocumentPipeline
-from images.pymupdf_image_extractor import PyMuPDFImageExtractor
-from ocr.tesseract_document_ocr import TesseractDocumentOCR
-from chunking.simple_chunker import SimpleChunker
-from prompting.simple_prompt_builder import SimplePromptBuilder
-from llm.ollama_llm import OllamaLLM
-from pipeline.base_pipeline import BasePipeline
-from core.config import OCRConfig, LLMConfig
-from ocr.tesseract_ocr import TesseractOCR
+from models.pipeline_result_model import PipelineResultModel
+# from images.pymupdf_image_extractor import PyMuPDFImageExtractor
+# from ocr.tesseract_document_ocr import TesseractDocumentOCR
+# from chunking.simple_chunker import SimpleChunker
+# from prompting.simple_prompt_builder import SimplePromptBuilder
+# from llm.ollama_llm import OllamaLLM
+# from pipeline.base_pipeline import BasePipeline
+# from core.config import OCRConfig, LLMConfig
+# from ocr.tesseract_ocr import TesseractOCR
 
-@pytest.fixture
-def ocr_config():
-    return OCRConfig
+# @pytest.fixture
+# def ocr_config():
+#     return OCRConfig
 
-@pytest.fixture
-def ocr_engine(ocr_config):
-    return TesseractOCR(ocr_config)
+# @pytest.fixture
+# def ocr_engine(ocr_config):
+#     return TesseractOCR(ocr_config)
 
-@pytest.fixture
-def llm_config():
-    return LLMConfig(
-        provider="ollama",
-        base_url="http://localhost:11434",
-        model_name="qwen2.5:3b",
-        temperature=0.1,
-        timeout=120
-    )
+# @pytest.fixture
+# def llm_config():
+#     return LLMConfig(
+#         provider="ollama",
+#         base_url="http://localhost:11434",
+#         model_name="qwen2.5:3b",
+#         temperature=0.1,
+#         timeout=120
+#     )
 
-@pytest.fixture
-def pipeline(ocr_engine, llm_config):
-    image_extractor = PyMuPDFImageExtractor()
-    ocr = TesseractDocumentOCR(ocr_engine)
-    chunker = SimpleChunker()
-    prompt_builder = SimplePromptBuilder()
-    llm = OllamaLLM(llm_config)
-    return DocumentPipeline(
+# @pytest.fixture
+# def pipeline(ocr_engine, llm_config):
+#     image_extractor = PyMuPDFImageExtractor()
+#     ocr = TesseractDocumentOCR(ocr_engine)
+#     chunker = SimpleChunker()
+#     prompt_builder = SimplePromptBuilder()
+#     llm = OllamaLLM(llm_config)
+#     return DocumentPipeline(
+#         image_extractor=image_extractor,
+#         ocr=ocr,
+#         chunker=chunker,
+#         prompt_builder=prompt_builder,
+#         llm=llm
+#     )
+
+# def test_document_pipeline_can_be_instantiated(pipeline):
+#     assert pipeline is not None
+
+# def test_document_pipeline_is_base_pipeline(pipeline):
+#     assert isinstance(pipeline, BasePipeline)
+
+# def test_document_pipeline_hold_dependencies(pipeline):
+    # assert isinstance(pipeline.image_extractor, PyMuPDFImageExtractor)
+    # assert isinstance(pipeline.ocr, TesseractDocumentOCR)
+    # assert isinstance(pipeline.chunker, SimpleChunker)
+    # assert isinstance(pipeline.prompt_builder, SimplePromptBuilder)
+    # assert isinstance(pipeline.llm, OllamaLLM)
+
+# def test_document_pipeline_process_calls_image_extractor():
+#     image_extractor = Mock()
+#     ocr = Mock()
+#     image_extractor.extract_images.return_value = "document_with_images"
+#     ocr.extract_document_text.return_value = "document_after_ocr"
+#     pipeline = DocumentPipeline(
+#         image_extractor=image_extractor,
+#         ocr=ocr,
+#         chunker=Mock(),
+#         prompt_builder=Mock(),
+#         llm=Mock()
+#     )
+#     result = pipeline.process("document")
+#     image_extractor.extract_images.assert_called_once_with("document")
+#     ocr.extract_document_text.assert_called_once_with("document_with_images")
+#     assert result == "document_after_ocr"
+
+# def test_document_pipeline_process_calls_ocr_after_image_extractor():
+
+#     image_extractor = Mock()
+#     ocr = Mock()
+#     image_extractor.extract_images.return_value = "document_with_images"
+#     ocr.extract_document_text.return_value = "document_with_text"
+#     pipeline = DocumentPipeline(
+#         image_extractor=image_extractor,
+#         ocr=ocr,
+#         chunker=Mock(),
+#         prompt_builder=Mock(),
+#         llm=Mock()
+#     )
+#     result = pipeline.process("document")
+#     image_extractor.extract_images.assert_called_once_with("document")
+#     ocr.extract_document_text.assert_called_once_with("document_with_images")
+#     assert result == "document_with_text"
+
+def test_document_pipeline_process_calls_chunker_after_ocr():
+    image_extractor = Mock()
+    ocr = Mock()
+    chunker = Mock()
+    prompt_builder = Mock()
+    image_extractor.extract_images.return_value = "document_with_images"
+    ocr.extract_document_text.return_value = "document_with_text"
+    chunker.chunk.return_value = "fake_chunk"
+    chunk1 = Mock()
+    chunk2 = Mock()
+    chunks = [chunk1, chunk2]
+    chunker.chunk.return_value = chunks
+    prompt_builder.build.side_effect = ["prompt1", "prompt2"]
+    llm = Mock()
+    llm.generate.side_effect = ["response1", "response2"]
+    pipeline = DocumentPipeline(
         image_extractor=image_extractor,
         ocr=ocr,
         chunker=chunker,
         prompt_builder=prompt_builder,
         llm=llm
     )
-
-def test_document_pipeline_can_be_instantiated(pipeline):
-    assert pipeline is not None
-
-def test_document_pipeline_is_base_pipeline(pipeline):
-    assert isinstance(pipeline, BasePipeline)
-
-def test_document_pipeline_hold_dependencies(pipeline):
-    assert isinstance(pipeline.image_extractor, PyMuPDFImageExtractor)
-    assert isinstance(pipeline.ocr, TesseractDocumentOCR)
-    assert isinstance(pipeline.chunker, SimpleChunker)
-    assert isinstance(pipeline.prompt_builder, SimplePromptBuilder)
-    assert isinstance(pipeline.llm, OllamaLLM)
-
+    result = pipeline.process("document")
+    image_extractor.extract_images.assert_called_once_with("document")
+    ocr.extract_document_text.assert_called_once_with("document_with_images")
+    chunker.chunk.assert_called_once_with("document_with_text")
+    prompt_builder.build.assert_any_call(chunk1)
+    prompt_builder.build.assert_any_call(chunk2)
+    assert prompt_builder.build.call_count == 2
+    assert llm.generate.call_count == 2
+    llm.generate.assert_any_call("prompt1")
+    llm.generate.assert_any_call("prompt2")
+    assert isinstance(result, PipelineResultModel)
